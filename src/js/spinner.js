39dbe7485d12d55d4b59f29f78ed6e09462b3d62
Bilogolovyi Sergii <sergey.hotchili@gmail.com>
2023-01-12 20:16:55 +0800

    added spinner (but commited it because now no proceses to use it)

---
+++ src/js/spinner.js
@@ -0,0 +1,9 @@
+const spinnerBoxRef = document.querySelector('.spinner-box');
+
+export function spinnerOff() {
+spinnerBoxRef.classList.add('none');
+}
+
+export function spinnerOn() {
+spinnerBoxRef.classList.remove('none');
+}
