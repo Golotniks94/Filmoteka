d655f311834205ce6e66011c9a8e91becd531ff9
Oleksandr Chernomorets <achernomorez@gmail.com>
2023-01-13 22:16:27 +0300

    check api

---
+++ src/js/apiServis.js
@@ -0,0 +1,83 @@
+const BASE_URL = `https://api.themoviedb.org/3`;
+const KEY = `c54d78350d3df927165feb0aabc91c13`;
+export default class NewApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+  fetchPopularArticles() {
+    const url = `${BASE_URL}/movie/popular?api_key=${KEY}&language=en-US&page=${this.page}`;
+    return fetch(url)
+      .then(response => response.json())
+      .then(({ results }) => {
+        return results;
+      });
+  }
+  fetchSearchArticles() {
+    const url = `${BASE_URL}/search/movie?api_key=${KEY}&language=en-US&page=${this.page}&query=${this.searchQuery}`;
+    return fetch(url)
+      .then(response => response.json())
+      .then(({ results }) => {
+        return results;
+      });
+  }
+  fetchPopularArticlesPages() {
+    const url = `${BASE_URL}/movie/popular?api_key=${KEY}&language=en-US&page=${this.page}`;
+    return fetch(url).then(response => response.json());
+  }
+  fetchSearchArticlesPages() {
+    const url = `${BASE_URL}/search/movie?api_key=${KEY}&language=en-US&page=${this.page}&query=${this.searchQuery}`;
+    return fetch(url).then(response => response.json());
+  }
+  fetchGenres() {
+    const url = `${BASE_URL}/genre/movie/list?api_key=${KEY}`;
+    return fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        return data.genres;
+      });
+  }
+  insertGenresToMovieObj() {
+    return this.fetchPopularArticles().then(data => {
+      return this.fetchGenres().then(genresList => {
+        return data.map(movie => ({
+          ...movie,
+          release_date: movie.release_date.split('-')[0],
+          genres: movie.genre_ids
+            .map(id => genresList.filter(el => el.id === id))
+            .flat(),
+        }));
+      });
+    });
+  }
+  insertGenresToSearchObj() {
+    return this.fetchSearchArticles().then(data => {
+      return this.fetchGenres().then(genresList => {
+        let release_date;
+        return data.map(movie => ({
+          ...movie,
+          release_date: movie.release_date
+            ? movie.release_date.split('-')[0]
+            : 'n/a',
+          genres: movie.genre_ids
+            ? movie.genre_ids
+                .map(id => genresList.filter(el => el.id === id))
+                .flat()
+            : 'n/a',
+        }));
+      });
+    });
+  }
+  get query() {
+    return this.searchQuery;
+  }
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+  get pageNum() {
+    return this.page;
+  }
+  set pageNum(newPage) {
+    this.page = newPage;
+  }
+}
