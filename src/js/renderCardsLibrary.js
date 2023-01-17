370ec9790a72ba58bcc322e4a1e9188c77ce70c4
Andruha0013 <andruha0013@gmail.com>
2023-01-17 16:03:47 +0200

    Library
    Зробив вивід з бібліотеки

--- src/js/renderCardsLibrary.js
+++ src/js/renderCardsLibrary.js
@@ -1,13 +1,13 @@
 //=========================render--functions==========================
 export default function renderCardV2(info){
     return info.map((info)=>
-    `<a href="${info.id}" class="movie-link">
-        <div class="gallery-card">
-            <img class="movie-img" src="https://image.tmdb.org/t/p/original${info.poster_path}" width="395" alt="${info.title}" loading="lazy"/>
+    `<li class="gallery-item">
+        <a href="${info.id}" class="movie-link">
+            <img class="movie-img" src="https://image.tmdb.org/t/p/original${info.poster_path}"  alt="${info.title}" loading="lazy"/>
             <h2 class="movie-title">${info.original_title}</h2>
-            <p class="movie-info">${getObjectGeners(info.genres)} <span>|</span> ${info.release_date.split('-')[0]}</p>
-        </div>
-    </a>`
+            <p class="movie-info">${getObjectGeners(info.genres)} <span>|</span> ${info.release_date.split('-')[0]}   </p>
+        </a>
+    </li>`
 ).join("");
 }
 
