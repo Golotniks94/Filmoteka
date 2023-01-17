5c4933ed4cde49c1cd89ce005282f15b21feab51
Olia Rozhniatovska <ola.rozhnyatovska@gmail.com>
2023-01-17 19:37:22 +0200

    updated

--- src/js/keyWordSearch.js
+++ src/js/keyWordSearch.js
@@ -1,99 +1,50 @@
-import { fetchSearchArticlesPages, fetchGenres } from './apiServis';
+import { Notify } from "notiflix/build/notiflix-notify-aio";
+import NewApiService from "./apiServis";
 import { createMovieCards } from './cardFetc';
-import Notiflix from 'Notiflix';
+import { pagination } from './pagination';
 
-const refs = {
-  moviesOnInputList: document.querySelector('.gallery'),
-  inputEl: document.querySelector('.form-search__input'),
-};
+const newApiService = new NewApiService();
 
-refs.inputEl.addEventListener('submit', searchHandler);
+const searchForm = document.querySelector('.form-search__icon');
+const inputEl = document.querySelector('.form-search__input');
 
-async function searchHandler(e) {
-  e.preventDefault();
-  const query = e.currentTarget.elements.searchQuery.value;
-  if (query === ' ' || query === '') {
-    Notiflix.Notify.failure('Please type search and try again.');
+inputEl.addEventListener('click', onSearchFormReset);
+searchForm.addEventListener('submit', onSearchFormSubmit);
+
+function onSearchFormReset() {
+  if (newApiService.query !== '') {
+    refs.searchForm.reset();
     return;
-  }
-  const data = await fetchSearchArticlesPages(query);
-  refs.moviesOnInputList.innerHTML = '';
-  if (data.total_results === 0) {
-    Notiflix.Notify.failure('There is no such film');
-    return;
-} else {
-    const genre = await fetchGenres().then(({ genres }) => {
-      if (data.results) {
-        data.results.forEach(movie => {
-          const { genre_ids, release_date } = movie;
-          genres.forEach(({ name, id }) => {
-            if (genre_ids.includes(id)) {
-              if (genre_ids.length > 2) {
-                genre_ids.splice(2, genre_ids.length - 1, 'Other');
-              }
-              genre_ids.splice(genre_ids.indexOf(id), 1, name);
-            }
-            movie.genre_names = genre_ids.join(', ');
-            if (movie.release_date) {
-              movie.release_date = release_date.slice(0, 4);
-            }
-          });
-        });
-      }
-    });
-
-    createMovieCards(data.results);
   }
 }
+export async function onSearchFormSubmit(e) {
+  e.preventDefault();
 
+  newApiService.page = 1;
+  newApiService.query = inputEl ? inputEl.value.trim() : '';
+  localStorage.setItem('input-value', newApiService.query);
 
-// import { Notify } from "notiflix/build/notiflix-notify-aio";
-// import NewApiService from "./apiServis";
-// import { createMovieCards } from './cardFetc';
-// import { pagination } from './pagination';
+  if (newApiService.query === '') {
+    return;
+  }
 
-// const newApiService = new NewApiService();
+  const results = await newApiService.getSearchFilms();
+  newApiService.totalResults = results.total_results;
+  try {
+    createMovieCards(results);
 
-// const searchForm = document.querySelector('.form-search__icon');
-// const inputEl = document.querySelector('.form-search__input');
+   pagination.reset(results.total_results);
 
-// inputEl.addEventListener('click', onSearchFormReset);
-// searchForm.addEventListener('submit', onSearchFormSubmit);
-
-// function onSearchFormReset() {
-//   if (newApiService.query !== '') {
-//     refs.searchForm.reset();
-//     return;
-//   }
-// }
-// export async function onSearchFormSubmit(e) {
-//   e.preventDefault();
-
-//   newApiService.page = 1;
-//   newApiService.query = inputEl ? inputEl.value.trim() : '';
-//   localStorage.setItem('input-value', newApiService.query);
-
-//   if (newApiService.query === '') {
-//     return;
-//   }
-
-//   const results = await newApiService.getSearchFilms();
-//   newApiService.totalResults = results.total_results;
-//   try {
-//     createMovieCards(results);
-
-//    pagination.reset(results.total_results);
-
-//     if (newApiService.totalResults === 0) {
-//       Notify.failure(
-//         'Sorry, there are no films matching your search query. Please try again.'
-//       );
-//       return;
-//     }
-//     if (newApiService.totalResults >= 1) {
-//       Notify.success(`Hooray! We found ${newApiService.totalResults} films.`);
-//     }
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
+    if (newApiService.totalResults === 0) {
+      Notify.failure(
+        'Sorry, there are no films matching your search query. Please try again.'
+      );
+      return;
+    }
+    if (newApiService.totalResults >= 1) {
+      Notify.success(`Hooray! We found ${newApiService.totalResults} films.`);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
