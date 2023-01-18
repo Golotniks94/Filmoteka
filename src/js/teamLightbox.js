be866bfd1dbbe4c6667b0b21161145d4aa8c2797
IvanHolovakha <meisuiwanreiser@gmail.com>
2023-01-18 21:07:14 +0200

    fixed backdrop scroll

--- src/js/teamLightbox.js
+++ src/js/teamLightbox.js
@@ -3,8 +3,14 @@ const modalTeam = document.querySelector("[data-team-modal]");
 const closeModalTeamBtn = document.querySelector(".team-modal__close-btn")
 
 openTeamModalLink.addEventListener("click", openTeamModal);
-closeModalTeamBtn.addEventListener("click", openTeamModal);
+closeModalTeamBtn.addEventListener("click", closeTeamModal);
 
 function openTeamModal (){
     modalTeam.classList.toggle("is-hidden");
+    document.body.style.position = 'fixed';
+}
+
+function closeTeamModal (){
+    modalTeam.classList.toggle("is-hidden");
+    document.body.style.position = '';
 }
