6bf5fd058feae94ab3ac1bbda21d46aee145fefd
IvanHolovakha <meisuiwanreiser@gmail.com>
2023-01-18 20:44:27 +0200

    delete console.logs

--- src/js/teamLightbox.js
+++ src/js/teamLightbox.js
@@ -1,13 +1,10 @@
 const openTeamModalLink = document.querySelector(".js-team-modal");
 const modalTeam = document.querySelector("[data-team-modal]");
 const closeModalTeamBtn = document.querySelector(".team-modal__close-btn")
-console.log(openTeamModalLink);
-console.log(modalTeam);
-console.log(closeModalTeamBtn);
+
 openTeamModalLink.addEventListener("click", openTeamModal);
 closeModalTeamBtn.addEventListener("click", openTeamModal);
 
 function openTeamModal (){
-    console.log(openTeamModalLink);
     modalTeam.classList.toggle("is-hidden");
 }
